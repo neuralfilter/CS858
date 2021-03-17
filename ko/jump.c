cf2a986ffd9e3896bd4880d1b2034ca44ba7f866
Bianca <esanubianca@gmail.com>
2021-03-17 19:49:22 -0400

    jump.c that works

--- ko/jump.c
+++ ko/jump.c
@@ -1,12 +1,26 @@
 #include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+#include <linux/kernel.h>   
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
-
-// When userspace writes a pointer to /proc/jump, jump to that address in
-// kernel mode.
-int jump_write(struct file *file, const char *buf,
-               unsigned long len, void *data) {
-    void (*fun)(void);
+#define BUFSIZE  100
+ 
+ 
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("Liran B.H");
+ 
+static int irq=20;
+module_param(irq,int,0660);
+ 
+static int mode=1;
+module_param(mode,int,0660);
+ 
+static struct proc_dir_entry *ent;
+ 
+static ssize_t mywrite(struct file *file, const char __user *buf, size_t len, loff_t *data) 
+{
+	void (*fun)(void);
 
     if (len < sizeof(fun))
         return -EINVAL;
@@ -19,23 +33,40 @@ int jump_write(struct file *file, const char *buf,
 
     return len;
 }
-
-// Create a file /proc/jump, with writes handled by jump_write.
-int init_jump(void) {
-    struct proc_dir_entry *ent = create_proc_entry("jump", 0666, NULL);
-    ent->write_proc = jump_write;
-
-    printk("jump.ko: Loaded incredibly insecure kernel module\n");
-    return 0;
+ 
+static ssize_t myread(struct file *file, char __user *ubuf,size_t count, loff_t *ppos) 
+{
+	char buf[BUFSIZE];
+	int len=0;
+	if(*ppos > 0 || count < BUFSIZE)
+		return 0;
+	len += sprintf(buf,"irq = %d\n",irq);
+	len += sprintf(buf + len,"mode = %d\n",mode);
+	
+	if(copy_to_user(ubuf,buf,len))
+		return -EFAULT;
+	*ppos = len;
+	return len;
 }
-
-void exit_jump(void) {
-    remove_proc_entry("jump", NULL);
+ 
+static struct file_operations myops = 
+{
+	.owner = THIS_MODULE,
+	.write = mywrite,
+};
+ 
+static int simple_init(void)
+{
+	ent=proc_create("jump",0664,NULL,&myops);
+	printk(KERN_ALERT "hello...\n");
+	return 0;
 }
-
-module_init(init_jump);
-module_exit(exit_jump);
-
-MODULE_AUTHOR("Keegan McAllister");
-MODULE_DESCRIPTION("Incredibly insecure kernel module for testing exploitation techniques");
-MODULE_LICENSE("Dual BSD/GPL");
+ 
+static void simple_cleanup(void)
+{
+	proc_remove(ent);
+	printk(KERN_WARNING "bye ...\n");
+}
+ 
+module_init(simple_init);
+module_exit(simple_cleanup);
